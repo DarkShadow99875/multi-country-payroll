3c7ec4d50f03877e8972f42d2b34eef30f3c2492
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-25 14:54:11 +0200

    Aggiunto CoreComponents placeholder

---
+++ lib/multi_country_payroll_web/components/core_components.ex
@@ -0,0 +1,6 @@
+defmodule MultiCountryPayrollWeb.CoreComponents do
+  use Phoenix.Component
+
+  # Add basic components here if needed
+  # For now this is a placeholder to satisfy the dependency
+end
