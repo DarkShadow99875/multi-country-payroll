83668187c19d46330df4b14bbc237952d4874494
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-25 13:37:16 +0200

    Aggiunto file multi_country_payroll_web.ex mancante

---
+++ lib/multi_country_payroll_web.ex
@@ -0,0 +1,37 @@
+defmodule MultiCountryPayrollWeb do
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+
+  def router do
+    quote do
+      use Phoenix.Router, helpers: false
+
+      import Plug.Conn
+      import Phoenix.Controller
+      import Phoenix.LiveView.Router
+    end
+  end
+
+  def live_view do
+    quote do
+      use Phoenix.LiveView,
+        layout: {MultiCountryPayrollWeb.Layouts, :app}
+
+      unquote(html_helpers())
+    end
+  end
+
+  def html_helpers do
+    quote do
+      import Phoenix.HTML
+      import Phoenix.HTML.Form
+      import Phoenix.Component
+
+      alias Phoenix.LiveView.JS
+      alias MultiCountryPayrollWeb.CoreComponents
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
