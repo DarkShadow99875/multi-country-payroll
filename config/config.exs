e34a3df9587e34e4adc2090732404bcd2a428b1d
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-25 12:48:45 +0200

    Aggiunta configurazione ChromicPDF

---
+++ config/config.exs
@@ -0,0 +1,39 @@
+import Config
+
+config :multi_country_payroll,
+  ecto_repos: [MultiCountryPayroll.Repo],
+  generators: [timestamp_type: :utc_datetime]
+
+config :phoenix, :json_library, Jason
+
+# PDF Export with ChromicPDF
+config :chromic_pdf,
+  chrome_path: System.get_env("CHROME_PATH") || "/usr/bin/google-chrome-stable"
+
+config :esbuild,
+  version: "0.25.0",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2020 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
+config :tailwind,
+  version: "4.0.9",
+  default: [
+    args: ~w(
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+      --watch
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+config :phoenix, :plug_init_mode, :runtime
+
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
