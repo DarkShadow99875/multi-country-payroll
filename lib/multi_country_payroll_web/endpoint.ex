2b1d8be484e414bcc72817ba79b122dab2bc89f1
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-25 15:32:46 +0200

    Creato Endpoint mancante

---
+++ lib/multi_country_payroll_web/endpoint.ex
@@ -0,0 +1,44 @@
+defmodule MultiCountryPayrollWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :multi_country_payroll
+
+  @session_options [
+    store: :cookie,
+    key: "_multi_country_payroll_key",
+    signing_salt: "dev_salt",
+    same_site: "Lax"
+  ]
+
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [connect_info: [session: @session_options]],
+    longpoll: [connect_info: [session: @session_options]]
+
+  plug Plug.Static,
+    at: "/",
+    from: :multi_country_payroll,
+    gzip: false,
+    only: MultiCountryPayrollWeb.static_paths()
+
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :multi_country_payroll
+  end
+
+  plug Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug MultiCountryPayrollWeb.Router
+end
