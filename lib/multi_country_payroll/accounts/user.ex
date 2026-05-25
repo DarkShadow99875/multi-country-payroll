b99507d245bd0b8481953a85cd967177ac5f8531
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-25 18:16:44 +0200

    Update user.ex

--- lib/multi_country_payroll/accounts/user.ex
+++ lib/multi_country_payroll/accounts/user.ex
@@ -4,17 +4,17 @@ defmodule MultiCountryPayroll.Accounts.User do
 
   schema "users" do
     field :email, :string
-    field :password, :string, virtual: true
     field :role, :string
-    field :company_id, :integer
-    field :employee_id, :integer
+    field :company_id, :id
+    field :password_hash, :string
 
     timestamps(type: :utc_datetime)
   end
 
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :password, :role, :company_id, :employee_id])
-    |> validate_required([:email, :role])
+    |> cast(attrs, [:email, :role, :company_id, :password_hash])
+    |> validate_required([:email, :role, :company_id])
+    |> unique_constraint(:email)
   end
 end
