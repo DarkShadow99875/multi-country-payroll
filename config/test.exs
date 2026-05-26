01ababa2d55872399de8f6d34273d348ef96b28a
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-26 12:02:54 +0200

    Update Oban test config to correct namespace

--- config/test.exs
+++ config/test.exs
@@ -7,8 +7,8 @@ config :multi_country_payroll, MultiCountryPayroll.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
-# Oban configuration for test (use in-memory or disable queues)
-config :oban, testing: :inline
+# Oban configuration for test (inline mode for fast tests)
+config :multi_country_payroll, Oban, testing: :inline
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
