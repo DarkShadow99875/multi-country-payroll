0f273eb44080b6326f729ecc1ba066fc383aea58
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-26 12:10:09 +0200

    Make DNSCluster optional to fix startup in test/CI

--- lib/multi_country_payroll/application.ex
+++ lib/multi_country_payroll/application.ex
@@ -5,12 +5,20 @@ defmodule MultiCountryPayroll.Application do
   def start(_type, _args) do
     children = [
       MultiCountryPayroll.Repo,
-      {DNSCluster, query: Application.get_env(:multi_country_payroll, :dns_cluster_query)},
       {Phoenix.PubSub, name: MultiCountryPayroll.PubSub},
       # Oban - Background Jobs
       {Oban, Application.fetch_env!(:multi_country_payroll, Oban)},
       MultiCountryPayrollWeb.Endpoint
     ]
+
+    # DNSCluster is optional (only start if query is configured)
+    dns_query = Application.get_env(:multi_country_payroll, :dns_cluster_query)
+    children =
+      if dns_query && dns_query != "" do
+        children ++ [{DNSCluster, query: dns_query}]
+      else
+        children
+      end
 
     opts = [strategy: :one_for_one, name: MultiCountryPayroll.Supervisor]
     Supervisor.start_link(children, opts)
