b4b44bad67a97a50d6e28eaf3ca2e5049974c7c1
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-28 11:46:32 +0200

    Add MFA fields to User schema

--- lib/multi_country_payroll/accounts/user.ex
+++ lib/multi_country_payroll/accounts/user.ex
@@ -8,12 +8,17 @@ defmodule MultiCountryPayroll.Accounts.User do
     field :company_id, :id
     field :password_hash, :string
 
+    # MFA fields
+    field :mfa_enabled, :boolean, default: false
+    field :mfa_secret, :string
+    field :mfa_method, :string, default: "email"
+
     timestamps(type: :utc_datetime)
   end
 
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :role, :company_id, :password_hash])
+    |> cast(attrs, [:email, :role, :company_id, :password_hash, :mfa_enabled, :mfa_secret, :mfa_method])
     |> validate_required([:email, :role, :company_id])
     |> unique_constraint(:email)
   end
