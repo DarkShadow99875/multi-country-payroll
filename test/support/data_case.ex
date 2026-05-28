8862f61050291fd28235ceda494479bd3d3ea5cc
DarkShadow99875 <128367162+DarkShadow99875@users.noreply.github.com>
2026-05-28 11:22:03 +0200

    Create missing test/support/data_case.ex

---
+++ test/support/data_case.ex
@@ -0,0 +1,39 @@
+defmodule MultiCountryPayroll.DataCase do
+  @moduledoc """
+  This module defines the setup for tests requiring
+  access to the application's data layer.
+
+  You may define functions here to be used as helpers in
+  your tests.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      alias MultiCountryPayroll.Repo
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+      import MultiCountryPayroll.DataCase
+    end
+  end
+
+  setup tags do
+    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(MultiCountryPayroll.Repo, shared: not tags[:async])
+    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    :ok
+  end
+
+  @doc """
+  A helper that transforms changeset errors into a map of messages.
+  """
+  def errors_on(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
+      Regex.replace(~r/%{\w+}/, message, fn _, key ->
+        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+      end)
+    end)
+  end
+end
